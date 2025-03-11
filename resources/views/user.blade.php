334ed71c8a24f09bedc9edb6426290d45f30512e
diana-rahma <98369663+diana-rahma@users.noreply.github.com>
2025-03-11 18:14:45 +0700

    prak 1 - prak 2.5

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -8,12 +8,14 @@
 </head>
 <body>
     <h1>Data User</h1>
+    <a href="/user/tambah">+ Tambah User</a>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            <th>ID</th>
-            <th>Username</th>
-            <th>Nama</th>
-            <th>ID Level Pengguna</th>
+            <td>ID</td>
+            <td>Username</td>
+            <td>Nama</td>
+            <td>ID Level Pengguna</td>
+            <td>Aksi</td>
         </tr>
         @foreach ($data as $d)
             <tr>
